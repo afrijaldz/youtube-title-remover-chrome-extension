0515a5f4f6c89cc106273974b24bd8bb19f58cc0
John Doe <ijuldz@gmail.com>
2021-02-25 16:12:12 +0700

    init

---
+++ popup.js
@@ -0,0 +1,33 @@
+let button = document.getElementById("remove-button");
+
+function YouTubeGetID(url) {
+  var ID = "";
+  url = url
+    .replace(/(>|<)/gi, "")
+    .split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  if (url[2] !== undefined) {
+    ID = url[2].split(/[^0-9a-z_\-]/i);
+    ID = ID[0];
+  } else {
+    ID = url;
+  }
+  return ID;
+}
+
+button.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  const youtubeID = YouTubeGetID(tab.url);
+
+  if (typeof youtubeID === "string") {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: removeYoutubeTitle,
+    });
+  }
+});
+
+function removeYoutubeTitle() {
+  document.title = "a";
+  document.getElementsByClassName("title")[0].children[0].innerText = "";
+}
